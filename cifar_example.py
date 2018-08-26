7744a0e1116f3ad4cbf3899170b4582d79361ed6
강준서 <karl2003@naver.com>
2018-08-26 21:22:23 +0900

    CIFAR10 Example

--- cifar_example.py
+++ cifar_example.py
@@ -1,5 +1,6 @@
 from InvalidNN import invalidnn as inv
 from InvalidNN.utill import pretreatment
+from InvalidNN.utill import test
 
 
 layers = [
@@ -10,19 +11,15 @@ layers = [
     inv.Conv2D('Conv_3', 'relu', 128, [3, 3], [1, 1], 'same'),
     inv.Conv2D('Conv_3', 'relu', 128, [3, 3], [1, 1], 'same'),
     inv.Conv2D('Conv_3', 'relu', 128, [3, 3], [1, 1], 'same'),
-    inv.Dense(name='Dense_1', activate_fn='sigmoid', units=384, dropout=True),
-    inv.Dense('Dense_2: Softmax', 'softmax', 10)
+    inv.Dense(name='Dense', activate_fn='sigmoid', units=384, dropout=True),
+    inv.Dense('Softmax', 'softmax', 10)
 ]
 
-
-train_data = [[x_train[i], y_train[i]] for i in range(len(x_train))]
-test_data = [[x_test[i], y_test[i]] for i in range(len(x_test))]
-
-train_data = pretreatment.one_hot(train_data, (0, 10), value=(0.01, 0.09))
-test_data = pretreatment.one_hot(test_data, (0, 10), value=(0.01, 0.09))
+train_data, test_data = pretreatment.cifar10_download('D:\Programming\Dataset\CIFAR-10')
 
 cifar_net = inv.NeuralNetwork(layers, [32, 32, 3])
 
 cifar_net.train(train_data, 128, 'cross-entropy', 'adam', 1e-4, dropout_p=0.7, epoch=10000)
 
-print('no running time err')
+acc = test.test_model(cifar_net, test_data)
+print('정확도: ' + acc)
