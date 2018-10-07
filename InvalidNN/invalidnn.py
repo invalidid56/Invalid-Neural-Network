e49719637a74aa1851130e95827016048a26dee1
강준서 <karl2003@naver.com>
2018-10-08 00:24:15 +0900

    Big Change: Initializing Layers
    현재는 전결합층 부분만

--- InvalidNN/invalidnn.py
+++ InvalidNN/invalidnn.py
@@ -9,24 +9,27 @@ from abc import *
 
 
 class Node(object):
-    def __init__(self, name, connected: list=None):
-        self.name: str = name
-        if not connected:
-            self.connected_node: list = connected[:-1]
-            self.gathering: str = connected[-1]  # TODO: 커스텀 자유도 높이기
+    def __init__(self, name, gathering=None):
+        self.name = name
+        if gathering:
+            if isinstance(gathering, tuple):
+                self.connected = gathering[:-1]
+                self.gathering = gathering[-1]
+            else:
+                self.connected = None
+                self.gathering = gathering
         else:
-            self.connected_node = []  # fully-connected to previous nodes
-            self.gathering = 'sum'
+            self.gathering = None
 
 
 class Layer(Node):
-    def __init__(self, name: str, activate: function, dropout: bool = False, connected: list=None):
-        super().__init__(name=name, connected=connected)
+    def __init__(self, name: str, activate, dropout: bool = False, gathering=None):
+        super().__init__(name=name, gathering=gathering)
         self.name = name
         self.activate = activate
         self.dropout = dropout
 
-        self.weight = None  # TODO: 생성 메서드 커스텀 추가
+        self.weight = None  # TODO: 생성 메서드 커스텀 추가, 프로퍼티?
         self.bias = None
 
     def __str__(self):
@@ -36,8 +39,8 @@ class Layer(Node):
 class Dense(Layer):
     category = 'Dense'
 
-    def __init__(self, name, activate, units, dropout=False, connected=None):
-        super().__init__(name=name, activate=activate, dropout=dropout, connected=connected)  # TODO: str 활성화함수 입력도 함수로 변환
+    def __init__(self, name, activate, units, dropout=False, gathering=None):
+        super().__init__(name=name, activate=activate, dropout=dropout, gathering=gathering)  # TODO: str 활성화함수 입력도 함수로 변환
         self.units = units
 
 
@@ -53,9 +56,6 @@ class WrongConnection(Exception):
 
 
 class NeuralNetwork(metaclass=ABCMeta):
-    def __init__(self, layers, input_shape):
-        pass
-
     @abstractmethod
     def init_layers(self, layers, input_shape):
         pass
@@ -72,13 +72,62 @@ class NeuralNetwork(metaclass=ABCMeta):
     def accuracy(self):
         pass
 
+
+class MixinNN(object):
     def __len__(self):
         pass
 
-    def __str__(self):
+    def __call__(self, *args, **kwargs):
         pass
 
 
+class TFNetwork(NeuralNetwork, MixinNN):
+    def __init__(self, layers, input_shape):
+        self.flow = self.init_layers(layers, input_shape)
+
+    def init_layers(self, layers, input_shape):
+        @dispatch(Dense, object)
+        def init(layer, in_flow):
+            #
+            if layer.gathering == 'concat':
+                input = tf.concat(1, in_flow)
+            elif layer.gathering == 'sum':
+                temp_flow = in_flow
+                input = in_flow[0]
+            else:
+                input = in_flow
+
+            #
+            layer.weight = tf.truncated_normal(shape=[
+                input.get_shape().as_list()[-1], layer.units
+            ]) / tf.sqrt(float(layer.units))
+            layer.bias = tf.truncated_normal(shape=[layer.units])
+
+            #
+            activate = lambda x: layer.activate(
+                tf.matmul(input, layer.weight) + layer.bias
+            )
+            return activate(input) if not layer.gathering == 'sum' else sum([activate(i) for i in temp_flow])
+
+        flow = tf.placeholder(tf.float32, input_shape)
+        for layer in layers:
+            if isinstance(layer, list):
+                flow = [init(node, flow) for node in layer]
+            else:
+                flow = init(layer, flow)
+        return flow
+
+    def query(self):
+        pass
+
+    def train(self):
+        pass
+
+    def accuracy(self):
+        pass
+
+
+'''
 class TFNetwork(NeuralNetwork):
     def init_layers(self, layers, input_shape):
         @dispatch()
@@ -86,27 +135,34 @@ class TFNetwork(NeuralNetwork):
             pass
 
         @dispatch(Dense)
-        def init(layer, in_flow):
-            # TODO: 초기화 메서드에 자유를! 자비에 말고도 사용자 지정을 받아오게 하자, 아예 각각 메서드들을 따로 모듈화해서 구현하고 이를 사용자가 직접 오버라이딩 할 수 있게 해도
-            layer.weight = tf.truncated_normal(
+        def init(layer: Dense, in_flow):
+            # TODO: 초기화 메서드에 자유도 자비에 말고도 사용자 지정을 받아오게 하자, 아예 각각 메서드들을 따로 모듈화해서 구현하고 이를 사용자가 직접 오버라이딩 할 수 있게 해도
 
-            )
-            layer.bias = tf.truncated_normal(
-
-            )
-            if isinstance(in_flow, list):
-                if layer.gathering == 'sum':
-                    for flow in in_flow:
-
-                result = None # TODO: Sum inputs
-            else:
+            if layer.gathering == 'concat':
+                in_flow = tf.concat(1, in_flow)
+            elif layer.gathering == 'sum':
+                temp = in_flow
+                in_flow = in_flow[0]
+            layer.weight = tf.truncated_normal(shape=[
+                        in_flow.get_shape().as_list()[-1], layer.units
+                    ])/tf.sqrt(layer.units)
+            layer.bias = tf.truncated_normal(shape=[layer.units])
+            if not layer.gathering == 'sum':
                 result = layer.activate(
                     tf.matmul(in_flow, layer.weight) + layer.bias
                 )
+            else:
+                result = sum([layer.activate(
+                    tf.matmul(i_f, layer.weight) + layer.bias
+                ) for i_f in temp])
             return result
+'''
 
 
 class PTNetwork(NeuralNetwork):
     def init_layers(self, layers, input_shape):
         pass
+
+
+# TODO: 리커런트?
 
--- examples/mnist_example.py
+++ examples/mnist_example.py
@@ -1,24 +1,14 @@
-from InvalidNN import invalidnn_new as inv
+from InvalidNN import invalidnn as inv
 from InvalidNN.utill import pretreatment
-from InvalidNN.utill import test
-import gzip
+import tensorflow as tf
 
-train_dataset, test_dataset = pretreatment.mnist_download()
+
+# train_dataset, test_dataset = pretreatment.mnist_download()
 
 sample_network = [
-    inv.Dense('fc_1', 'sigmoid', 200, dropout=True),
-    inv.Dense('fc_2', 'softmax', 10)
+    inv.Dense('fc_1', tf.nn.sigmoid, 200),
+    [inv.Dense('fc_2', tf.nn.softmax, 10), inv.Dense('fc_3', tf.nn.softmax, 10)],
+    inv.Dense('fc_4', tf.nn.softmax, 10, gathering='sum')
 ]
 
-mynet = inv.NeuralNetwork(sample_network, [None, 784])
-
-mynet.train(
-    train_dataset,
-    batch_size=100,
-    loss_fn='least-square',
-    optimizer='gradient-descent',
-    learning_rate=0.05,
-    epoch=5000,
-    model_path='.',
-    summary_path='./summary',
-    drop_p=0.7)
+mynet = inv.TFNetwork(sample_network, [None, 784])
